fc98a4f719da01e50828edc36e54c65c8cc24322
Faakhar Habeeb <faakhar.hzb@gmail.com>
2025-03-09 15:08:55 +0500

    Updated utilities.py

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -2,11 +2,6 @@ import pygame as pg
 import time
 from os import listdir, path
 
-def fps_independence(last_time: float) -> float:
-    dt = time.time() - last_time
-    dt *= 60
-    return dt
-
 def load_image(image_path: str, colorkey: int = None) -> pg.Surface:
     image = pg.image.load(image_path).convert()
     image.set_colorkey(colorkey)
@@ -16,6 +11,7 @@ def load_images(image_dir: str, colorkey: int = None) -> list[pg.Surface]:
     return [load_image(image_path, colorkey) for image_path in sorted(listdir(image_dir))]
 
 
-def show_text(font_size: int, pos: int, color: int, bold: bool = False, italic: bool = False, text: str):
-    font = pg.font.SysFont('arial', font_size, bold, italic)
-		 font.render(text, True, color)
+def show_text(font: pg.font.Font, pos: int, color: pg.Color, text: str, surf: pg.Surface):
+		 text_surf = font.render(text, True, color)
+    surf.blit(text_surf, pos)
+    
